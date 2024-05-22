ae4c764930e953b410acf8bace21a2c603356bfb
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-22 19:49:12 -0400

    Initial logic for calculating tip using ternary operator

---
+++ cc_2.js
@@ -0,0 +1,4 @@
+// Tip Calculator
+let bill = 275; // Example bill amount
+let tip = (bill > 50 & bill = 300) bill * .1 : bill * 0.2;
+console.log(`Initial logic for calculating tip using ternary operator: Bill = ${bill}, Tip = ${tip}`);
