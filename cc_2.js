8ba2b82e27171716bdc405cec190fc6a95fc2084
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-22 22:27:22 -0400

    Create and test calcTip function

--- cc_2.js
+++ cc_2.js
@@ -10,3 +10,7 @@ const billAmount = 275;
 const tipAmount = calculateTip(billAmount);
 const totalAmount = billAmount + tipAmount;
 console.log(`The bill was $${billAmount}, the tip was $${tipAmount.toFixed(2)}, total cost (bill plus tip) was $${totalAmount.toFixed(2)}`);
+
+function calcTip(bill) {
+    return bill > 300 ? bill * .20 : bill * .15;
+}
