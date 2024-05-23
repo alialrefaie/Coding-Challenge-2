0469398ad4355c838f17cae876e40bddcd9e2dcc
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-22 21:51:55 -0400

    Fixed initial logic for calculating tip using ternary operator and fixed functionality to display bill, tip, and total

--- cc_2.js
+++ cc_2.js
@@ -1,9 +1,12 @@
 // Tip Calculator
-let bill = 275; // Example bill amount
-let tip = (bill > 50 & bill = 300) bill * .1 : bill * 0.2;
-console.log(`Initial logic for calculating tip using ternary operator: Bill = ${bill}, Tip = ${tip}`);
 
-const bill = 275;
-const tip = calculateTip(bill);
-const total = bill + tip;
-console.log(`The bill was ${bill}, the tip was ${tip.toFixed(2)}, total cost (bill+tip) was ${totalAmount.toFixed(2)}`);
+function calculateTip(bill) {
+
+const tip = bill > 300 ? bill * .20 : bill * 0.15;
+return tip;
+}
+
+const billAmount = 275;
+const tipAmount = calculateTip(billAmount);
+const totalAmount = billAmount + tipAmount;
+console.log(`The bill was $${billAmount}, the tip was $${tipAmount.toFixed(2)}, total cost (bill plus tip) was $${totalAmount.toFixed(2)}`);
