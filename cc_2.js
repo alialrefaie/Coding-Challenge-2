1987653f7a12f912e41fe0c257a53a5b1960cae5
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-22 21:13:49 -0400

    Added output functionality to display bill, tip, and total

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,8 @@
 let bill = 275; // Example bill amount
 let tip = (bill > 50 & bill = 300) bill * .1 : bill * 0.2;
 console.log(`Initial logic for calculating tip using ternary operator: Bill = ${bill}, Tip = ${tip}`);
+
+const bill = 275;
+const tip = calculateTip(bill);
+const total = bill + tip;
+console.log(`The bill was ${bill}, the tip was ${tip.toFixed(2)}, total cost (bill+tip) was ${totalAmount.toFixed(2)}`);
